0a274dda534ce7c5d4cc87318ee871bac7be22ca
Shachar <shar535909@gmail.com>
2025-07-23 15:33:58 +0300

    Walla, I built an image and a container in Docker V1.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 
 
 if __name__ =="__main__":
-    uvicorn.run("server.server:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("server.server:app", host="127.0.0.1", port=8001, reload=True)
 
 
 
